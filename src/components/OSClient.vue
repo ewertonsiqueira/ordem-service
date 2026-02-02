9242e873a9b4851f04f2bf76981020cd0aea34c2
Ewerton Siqueira <ewertoncostaxx@hotmail.com>
2026-02-02 18:31:22 -0300

    add OSClient component with initial template

---
+++ src/components/OSClient.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row no-gutters>
+		<v-col>
+			<div>
+				<H1>ORDEM DE SERVIÇO</H1>
+				<div>Rua Pastor Lioeses Domiciano, 256</div>
+				<div>Joinville - SC</div>
+				<div>62 985921947</div>
+				<div>Email: </div>
+			</div>
+		</v-col>
+		<v-col>
+			
+		</v-col>
+	</v-row>
+</template>
