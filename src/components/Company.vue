91e21745ce377f923f24c05598f52509091d4e7b
Ewerton Siqueira <ewerton@rotaexata.com.br>
2026-02-02 23:38:49 -0300

    rename componente de dados da empresa

---
+++ src/components/Company.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span class="title">ORDEM DE SERVIÇO</span>
+    <div>Rua Pastor Lioeses Domiciano, 256</div>
+    <div>Joinville - SC</div>
+    <div>62 985921947</div>
+    <div>Email: </div>
+  </div>
+</template>
+<style>
+.title {
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
