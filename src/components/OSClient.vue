03ce9fdede1d3a0b6b72db5c9a346b73870d0e3f
Ewerton Siqueira <ewerton@rotaexata.com.br>
2026-02-03 16:29:47 -0300

    ajustes nos componentes

--- src/components/OSClient.vue
+++ src/components/OSClient.vue
@@ -1,16 +1,57 @@
 <template>
-  <v-row no-gutters>
-		<v-col>
-			<div>
-				<H1>ORDEM DE SERVIÇO</H1>
-				<div>Rua Pastor Lioeses Domiciano, 256</div>
-				<div>Joinville - SC</div>
-				<div>62 985921947</div>
-				<div>Email: </div>
-			</div>
-		</v-col>
-		<v-col>
-			
-		</v-col>
-	</v-row>
+  <v-card class="pa-4" variant="outlined">
+    <v-card-title class="pb-2" style="font-weight: bold; color: #f1b600; font-size: 18px;">
+      Dados do Cliente
+    </v-card-title>
+    <v-card-text>
+      <v-row>
+        <v-col cols="6">
+          <v-text-field
+            v-model="client.name"
+            variant="outlined"
+            density="comfortable"
+            label="Nome Completo"
+            persistent-placeholder
+          />
+        </v-col>
+        <v-col cols="6">
+          <v-text-field
+            v-model="client.phone"
+            variant="outlined"
+            density="comfortable"
+            label="Telefone / WhatsApp"
+            persistent-placeholder
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6">
+          <v-text-field
+            v-model="client.city"
+            variant="outlined"
+            density="comfortable"
+            label="Cidade"
+            persistent-placeholder
+          />
+        </v-col>
+        <v-col cols="6">
+          <v-text-field
+            v-model="client.state"
+            variant="outlined"
+            density="comfortable"
+            label="Estado"
+            persistent-placeholder
+          />
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
 </template>
+<script setup>
+import { VRow, VCol, VTextField } from 'vuetify/components';
+import { useServiceOrderStore } from '../store/serviceOrder'
+import { storeToRefs } from 'pinia';
+
+const serviceOrderStore = useServiceOrderStore()
+const { client } = storeToRefs(serviceOrderStore)
+</script>
