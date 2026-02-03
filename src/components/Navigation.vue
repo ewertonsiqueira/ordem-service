fec1a8e0ceae42e94a903efa34824ddbd97fa167
Ewerton Siqueira <ewerton@rotaexata.com.br>
2026-02-02 23:19:18 -0300

    refactor: clean up Navigation.vue template and script formatting

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,33 +1,33 @@
 <template>
-  <v-container fluid >
-    <v-navigation-drawer
-      class="bg-deep-purple"
-      theme="dark"
-      permanent
-    >
-      <v-list color="transparent">
-        <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard"></v-list-item>
-      </v-list>
+  <!-- <v-container fluid> -->
+  <v-navigation-drawer
+    class="bg-deep-purple"
+    theme="dark"
+    permanent
+  >
+    <v-list color="transparent">
+      <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard" />
+    </v-list>
 
-      <template v-slot:append>
-        <div class="pa-2">
-          <v-btn block>
-            Logout
-          </v-btn>
-        </div>
-      </template>
-    </v-navigation-drawer>
-    <v-main>
-      <slot></slot>
-    </v-main>
-  </v-container>
+    <template #append>
+      <div class="pa-2">
+        <v-btn block>
+          Logout
+        </v-btn>
+      </div>
+    </template>
+  </v-navigation-drawer>
+  <v-main>
+    <slot />
+  </v-main>
+  <!-- </v-container> -->
 </template>
 <script setup>
-	import {
-		VMain,
-		VNavigationDrawer,
-		VList,
-		VListItem,
-		VBtn
-	} from 'vuetify/components';
+import {
+  VMain,
+  VNavigationDrawer,
+  VList,
+  VListItem,
+  VBtn
+} from 'vuetify/components';
 </script>
