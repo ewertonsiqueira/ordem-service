1cb189cd8429e1b69ef124e73ed105a00ae9136d
Ewerton Siqueira <ewerton@rotaexata.com.br>
2026-02-04 22:21:52 -0300

    refactor: restructure HomeView layout to use v-row and v-col for better responsiveness

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="d-flex">
-    <OSClient />
-    <OSDetails />
-  </div>
+  <v-row class="d-flex ma-5">
+    <v-col cols="6">
+      <Company />
+    </v-col>
+    <v-col cols="6" class="d-flex flex-column justify-start">
+      <OSDetails />
+    </v-col>
+  </v-row>
+  <v-row class="ma-5">
+    <v-col cols="12">
+      <OSClient />
+    </v-col>
+  </v-row>
 </template>
 <script setup>
+import Company from '../components/Company.vue';
 import OSClient from '../components/OSClient.vue';
 import OSDetails from '../components/OSDetails.vue';
 
