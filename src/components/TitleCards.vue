45e0dea76584fc92eb29d4a4c47f49f26425a870
Ewerton Siqueira <ewerton@rotaexata.com.br>
2026-02-08 15:13:30 -0300

    criando componente de title para reutilizacao

---
+++ src/components/TitleCards.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="title">
+    {{ title }}
+  </div>
+</template>
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+<style scoped>
+.title {
+  font-weight: bold;
+  font-size: 1.25rem;
+  margin-bottom: 16px;
+  background-color: #DFDFDF;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+}
+</style>
